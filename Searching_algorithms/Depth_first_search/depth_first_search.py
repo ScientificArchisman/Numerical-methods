882e214548388fd9d69a5748a210f40af39960e5
Archisman <archismanninja@gmail.com>
2023-12-13 22:06:47 +0530

    added DFS algorithm

---
+++ Searching_algorithms/Depth_first_search/depth_first_search.py
@@ -0,0 +1,56 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+plt.style.use(["science", "notebook", "grid"])
+
+
+edge_list = []
+for _ in range(100):
+    node1, node2 = np.random.randint(low = 0, high = 50, size = 2)
+    edge_list.append((node1, node2))
+
+
+G = nx.Graph()
+G.add_edges_from(edge_list)
+
+
+def dfs(graph, start_node, target_node, visited=None):
+    if visited is None:
+        visited = []
+    visited.append(start_node)
+    if start_node == target_node:
+        return visited
+    for neighbor in graph.neighbors(start_node):
+        if neighbor not in visited:
+            path = dfs(graph, neighbor, target_node, visited)
+            if path is not None:
+                return path
+    return None
+
+def visualize_search(graph, order, title, pos):
+    plt.title(title, fontsize = 15, fontweight = "bold")
+    visited = []
+    for idx, node in enumerate(order):
+        plt.clf()
+        plt.title(title, fontsize = 15, fontweight = "bold")
+        visited.append(node)
+        colors = []
+        for n in graph.nodes:
+            if n in visited:
+                colors.append("r")
+            else:
+                colors.append("g")
+        nx.draw(G, pos, with_labels = True, 
+                node_color = colors)
+        plt.draw()
+        plt.pause(0.5)
+    plt.show()
+    time.sleep(1)
+
+
+
+pos = nx.spring_layout(G)
+print(f"Starting node: {node1}, Target node: {node2}")
+order = dfs(graph=G, start_node=node1, target_node=node2)
+visualize_search(G, order, "DFS", pos)
